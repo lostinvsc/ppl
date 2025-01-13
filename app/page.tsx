c86d16ca56b84926841f3bce77a562be02787bfb
sachin <sachinnagar2004@gmail.com>
2025-01-13 20:27:16 +0530

    second commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,10 @@
 import Hero from "@/components/Hero";
 import Grid from "@/components/Grid";
 import Footer from "@/components/Footer";
-import Clients from "@/components/Clients";
 import Approach from "@/components/Approach";
 import Experience from "@/components/Experience";
 import RecentProjects from "@/components/RecentProjects";
-import { FloatingNav } from "@/components/ui/FloatingNavbar";
+
 
 const Home = () => {
   return (
